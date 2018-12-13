2064b9f1283ae5c56577c0db3d8f8a963d73abe5
fengjiling <jiling.feng@kangcenet.com>
2018-12-13 09:48:24 +0800

    添加.gitignore

--- userblog/database.js
+++ userblog/database.js
@@ -1,4 +1,11 @@
 
 var mongoose=require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/blog")
+
+mongoose.connect("mongodb://localhost:27017/blog",{useMongoClient:true},function (err) {
+    if(err){
+        console.log("数据库连接失败！");
+    }else{
+        console.log("数据库连接成功！");
+    }
+})
